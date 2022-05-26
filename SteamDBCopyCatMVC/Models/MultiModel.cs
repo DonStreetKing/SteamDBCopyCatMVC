27b63c609c0feb74ed1ef61056fa618a870a0194
DonStreetKing <donstreetking@gmail.com>
2022-05-26 13:06:25 +0700

    ........

---
+++ SteamDBCopyCatMVC/Models/MultiModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SteamDBCopyCatMVC.EDMX;
+namespace SteamDBCopyCatMVC.Models
+{
+    public class MultiModel
+    {
+        public IEnumerable<TabelBarang> TabelBarangs { get; set; }
+        public IEnumerable<Akun> Akuns { get; set; }
+    }
+}
